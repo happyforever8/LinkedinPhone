36801f1b11040ef904cab868c7ca2ff34eeb76dc
happyforever8 <83077456+happyforever8@users.noreply.github.com>
2021-08-26 08:20:58 -0400

    Update RetainBestCache.java

--- RetainBestCache.java
+++ RetainBestCache.java
@@ -1,3 +1,45 @@
+===== Questions======
+
+
+public class RetainBestCache<K, T extends Rankable> {
+    int entriesToRetain;
+    HashMap<K, T> map = new HashMap<K,T>();
+    DataSource<K,T> ds;
+
+/* Constructor with a data source (assumed to be slow) and a cache size */
+public RetainBestCache(DataSource<K,T> ds, int entriesToRetain) {
+    //impliment here
+    }
+    /* Gets some data. If possible, retrieves it from cache to be fast. If the data is not cached,
+    * retrieves it from the data source. If the cache is full, attempt to cache the returned data,
+    * evicting the T with lowest rank among the ones that it has available
+    * If there is a tie, the cache may choose any T with lowest rank to evict.
+    */
+public T get(K key) {
+    //impliment here
+    }
+    /*
+    * For reference, here are the Rankable and DataSource in‍‍‍‍‍‍‍‍‌‍‌‌‍‌‌‌‌‌terfaces.
+    * You do not need to implement them, and should not make assumptions
+    * about their implementations.
+    */
+public interface Rankable {
+    /**
+    * Returns the Rank of this object, using some algorithm and potentially
+    * the internal state of the Rankable.
+    */
+long getRank();
+}
+public interface DataSource<K, T extends Rankable> {
+    T get(K key);
+}
+
+
+
+========== Answer================
+
+
+
 public class RetainBestCache<K, T extends Rankable> {
 
      private Map<K, T> cache;
